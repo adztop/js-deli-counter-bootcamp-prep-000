94b714acea2a49d1df0f4fed87710dba71cd8fc9
Rinoa Heartilly <kissy0227@yahoo.com.au>
2018-08-09 23:23:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function takeANumber(katzDeliLine,name){
   katzDeliLine.push(`${name}`)
   return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
 }
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return ""
+  }
+  return katzDeliLine.
+}
