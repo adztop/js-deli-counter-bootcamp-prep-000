5f1eae6fd75c345b8e20402e39ef8d5dd2a4b8fe
Rinoa Heartilly <kissy0227@yahoo.com.au>
2018-08-09 23:12:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var katzDeliLine = []
 
 function takeANumber(n,name){
-  katzDeliLine.push(name)
+  katzDeliLine.push(`${name}`)
   n = katzDeliLine.length
   return (`Welcome, ${name}. You are number ${n} in line.`)
 }
